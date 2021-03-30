6efce14b2545dade4dfe0bd54a19d858fa13301b
Vlad Afonin <mytecor@ya.ru>
2021-03-31 01:34:56 +0300

    Now we can use Resolvable as constructor
    In example:
    let request = new Resolvable
    request.resolve('Hey')

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const makeResolvable = (promise) => {
+function Resolvable(handler) {
   let resolve, reject
   const result = new Promise(async (res, rej) => {
     resolve = res
     reject = rej
 
-    if (typeof promise === "function") {
+    if(typeof handler == 'function')
       promise(res, rej)
-    }
-    else if (promise instanceof Promise) {
-      promise.then(res, rej)
-    }
+    else if(handler instanceof Promise)
+      handler.then(res, rej)
   })
 
   result.resolve = resolve
@@ -17,4 +15,4 @@ const makeResolvable = (promise) => {
   return result
 }
 
-module.exports = makeResolvable
+module.exports = Resolvable
